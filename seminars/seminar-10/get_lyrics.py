f56fd37f6ebfe64d55c72b582fd898b4e41b6a66
MerkulovDaniil <10253272+MerkulovDaniil@users.noreply.github.com>
2023-04-18 12:40:13 +0300

    add some texts

---
+++ seminars/seminar-10/get_lyrics.py
@@ -0,0 +1,33 @@
+# GET IT HERE 
+# https://docs.genius.com/#/undefined
+client_access_token = ""
+
+import re
+import lyricsgenius
+from pathlib import Path
+
+def clean_up(lyrics):
+    junkwords = ["[Припев", "[Текст", "[Куплет", "[Предприпев", "[Интро", 
+    "[Аутро", "[Рефрен", "[Интерлюдия", "[Постприпев", "[Бридж", "[Переход",
+    "[Скит"]
+    clean_lyrics = ''
+    for line in lyrics.splitlines():
+        if all(junk not in line for junk in junkwords):
+            clean_lyrics += line + '\n'
+
+    clean_lyrics = re.sub(r'\d+Embed','',clean_lyrics)
+    
+    return clean_lyrics
+
+import lyricsgenius
+LyricsGenius = lyricsgenius.Genius(client_access_token)
+
+artist = LyricsGenius.search_artist("Manowar", max_songs=300)
+lyrics = ''
+
+for song in artist.songs:
+    lyrics += clean_up(song.lyrics)
+    lyrics += '\n\n[EOS]\n\n'
+
+    with open("manowar_lyrics.txt", "w") as text_file:
+        text_file.write(lyrics)
